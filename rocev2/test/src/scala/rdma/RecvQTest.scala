6fffeb1952d9799ffe5232623064106ff29f1580
hy0000 <2877079184@qq.com>
2021-11-27 09:51:14 +0800

    fix NO DRIVER on 'qp/rq/io_qpAttr'

--- rocev2/test/src/scala/rdma/RecvQTest.scala
+++ rocev2/test/src/scala/rdma/RecvQTest.scala
@@ -10,16 +10,19 @@ import scala.collection.mutable
 import scala.tools.nsc.doc.base.comment.Body
 import scala.util.Random
 
-/**
- * The SeqOut module can be divided into two channels
- * -- rxOtherRespSort channel sorts the packet form io.rxOtherResp. We can firstly verify
- *    this channel by set io.rxReadResp.valid false so that we can focus on sorting function.
- *
- * -- rxReadResp channel is simple. When above is verified, test the arbitration logic
- */
+/** The SeqOut module can be divided into two channels
+  * -- rxOtherRespSort channel sorts the packet form io.rxOtherResp. We can firstly verify
+  *    this channel by set io.rxReadResp.valid false so that we can focus on sorting function.
+  *
+  * -- rxReadResp channel is simple. When above is verified, test the arbitration logic
+  */
 class SeqOutTest extends AnyFunSuite {
 
-  class RxDriver(rx: Stream[Fragment[RdmaDataBus]], clockDomain: ClockDomain, fragments: Int) {
+  class RxDriver(
+      rx: Stream[Fragment[RdmaDataBus]],
+      clockDomain: ClockDomain,
+      fragments: Int
+  ) {
     val psnQueue = new mutable.Queue[Int]
     rx.valid #= false
     rx.last #= false
@@ -52,7 +55,11 @@ class SeqOutTest extends AnyFunSuite {
     }
   }
 
-  class PsnSetter(qpAttr: QpAttrData, qpAttrUpdate: Stream[Bits], clockDomain: ClockDomain) {
+  class PsnSetter(
+      qpAttr: QpAttrData,
+      qpAttrUpdate: Stream[Bits],
+      clockDomain: ClockDomain
+  ) {
     qpAttrUpdate.valid #= false
     qpAttrUpdate.payload #= QpAttrMask.QP_RQ_PSN.id
 
@@ -65,10 +72,8 @@ class SeqOutTest extends AnyFunSuite {
     }
   }
 
+  // use buffer depth as max distance
   class PsnGenerator(maxDistance: Int) {
-    // maxDistance define:
-    // [1 3 2 4] max distance is 2
-    // [1 3 2 4 6 7 5] max distance is 3
     var PSN: Int = -1
 
     def setPsn(psn: Int): this.type = {
@@ -77,6 +82,7 @@ class SeqOutTest extends AnyFunSuite {
     }
 
     def genNextDisorder(n: Int): List[Int] = {
+      // generate disorder PSNs which do not contribute dead stuck
       assert(PSN != (-1))
       var psnList = (PSN until PSN + n).toList
       var moved = List.fill(n)(false)
@@ -104,15 +110,16 @@ class SeqOutTest extends AnyFunSuite {
     }
   }
 
-  def setInputDrivers(dut: SeqOut): (RxDriver, RxDriver, PsnSetter) ={
+  def setInputDrivers(dut: SeqOut): (RxDriver, RxDriver, PsnSetter) = {
     val rxOtherDriver = new RxDriver(dut.io.rxOtherResp, dut.clockDomain, 1)
     val rxReadDriver = new RxDriver(dut.io.rxReadResp, dut.clockDomain, 8)
-    val psnSetter = new PsnSetter(dut.io.qpAttr, dut.io.qpAttrUpdate, dut.clockDomain)
+    val psnSetter =
+      new PsnSetter(dut.io.qpAttr, dut.io.qpAttrUpdate, dut.clockDomain)
     (rxOtherDriver, rxReadDriver, psnSetter)
   }
 
-  def waitTargetPsn(dut: SeqOut, psnList: List[Int]): Unit ={
-    for(psn <- psnList){
+  def waitTargetPsn(dut: SeqOut, psnList: List[Int]): Unit = {
+    for (psn <- psnList) {
       waitTargetPsn(dut, psn)
     }
   }
@@ -128,12 +135,13 @@ class SeqOutTest extends AnyFunSuite {
       timeOut -= 1
       dut.clockDomain.waitRisingEdge()
       if (timeOut == 0) {
-        simFailure(Seq(
-          s"sim time out when waiting psn = ${psn}, opsnReg = ${dut.opsnReg.toInt}",
-          s"rxOtherResp.psn = ${dut.io.rxOtherResp.bth.psn.toInt}, valid ${dut.io.rxOtherResp.valid.toBoolean}, ready ${dut.io.rxOtherResp.ready.toBoolean}",
-          s"rxReadResp.psn = ${dut.io.rxReadResp.bth.psn.toInt}, valid ${dut.io.rxReadResp.valid.toBoolean}, ready ${dut.io.rxReadResp.ready.toBoolean}",
-          s"tx.psn          = ${dut.io.tx.bth.psn.toInt}, valid ${dut.io.tx.valid.toBoolean}, ready ${dut.io.tx.ready.toBoolean}",
-        ).reduce((a, b) => a ++ "\n" ++ b)
+        simFailure(
+          Seq(
+            s"sim time out when waiting psn = ${psn}",
+            s"rxOtherResp.psn = ${dut.io.rxOtherResp.bth.psn.toInt}, valid ${dut.io.rxOtherResp.valid.toBoolean}, ready ${dut.io.rxOtherResp.ready.toBoolean}",
+            s"rxReadResp.psn = ${dut.io.rxReadResp.bth.psn.toInt}, valid ${dut.io.rxReadResp.valid.toBoolean}, ready ${dut.io.rxReadResp.ready.toBoolean}",
+            s"tx.psn          = ${dut.io.tx.bth.psn.toInt}, valid ${dut.io.tx.valid.toBoolean}, ready ${dut.io.tx.ready.toBoolean}"
+          ).reduce((a, b) => a ++ "\n" ++ b)
         )
       }
     }
@@ -188,7 +196,11 @@ class SeqOutTest extends AnyFunSuite {
     assert(dut.io.rxOtherResp.ready.toBoolean)
   }
 
-  def simRandomBase(dut: SeqOut, bufferDepth: Int, simTask:(RxDriver, RxDriver, PsnGenerator, Int)=>Unit): Unit ={
+  def simRandomBase(
+      dut: SeqOut,
+      bufferDepth: Int,
+      simTask: (RxDriver, RxDriver, PsnGenerator, Int) => Unit
+  ): Unit = {
     dut.clockDomain.forkStimulus(2)
     fork {
       while (true) {
@@ -196,30 +208,34 @@ class SeqOutTest extends AnyFunSuite {
         dut.clockDomain.waitRisingEdge(Random.nextInt(bufferDepth))
       }
     }
-    val (rxOtherDriver, rxReadRespDriver, psnSetter) = setInputDrivers(dut)
+    val (rxOtherDriver, rxReadDriver, psnSetter) = setInputDrivers(dut)
     val psnGen = new PsnGenerator(bufferDepth)
 
     for (_ <- 0 to 100) {
       // the psn width is 24, so...
-      val psn = Random.nextInt(0xFFFFFF)
-      var packetNum = Random.nextInt(0xFFF)
-      if (psn + packetNum > 0xFFFFFF) {
-        packetNum = 0xFFFFFF - psn
+      val psn = Random.nextInt(0xffffff)
+      var packetNum = Random.nextInt(0xfff)
+      if (psn + packetNum > 0xffffff) {
+        packetNum = 0xffffff - psn
       }
       psnSetter.set(psn)
       psnGen.setPsn(psn)
-      simTask(rxOtherDriver, rxReadRespDriver, psnGen, packetNum)
+      simTask(rxOtherDriver, rxReadDriver, psnGen, packetNum)
       waitTargetPsn(dut, (psn until psn + packetNum).toList)
     }
   }
 
   def simRxOtherRespRandom(dut: SeqOut, bufferDepth: Int): Unit = {
-    simRandomBase(dut, bufferDepth, (rxOtherDriver, rxReadRespDriver, psnGen, packetNum)=>{
-      rxOtherDriver.push(psnGen.genNextDisorder(packetNum))
-    })
+    simRandomBase(
+      dut,
+      bufferDepth,
+      (rxOtherDriver, rxReadRespDriver, psnGen, packetNum) => {
+        rxOtherDriver.push(psnGen.genNextDisorder(packetNum))
+      }
+    )
   }
 
-  def simSeqOut(dut:SeqOut, bufferDepth:Int): Unit ={
+  def simSeqOut(dut: SeqOut, bufferDepth: Int): Unit = {
     dut.clockDomain.forkStimulus(2)
     val (rxOtherDriver, rxReadDriver, psnSetter) = setInputDrivers(dut)
     val psnGen = new PsnGenerator(bufferDepth)
@@ -239,7 +255,7 @@ class SeqOutTest extends AnyFunSuite {
     rxReadDriver.push(psnGen.genNextOrder(10))
     rxOtherDriver.push(psnGen.genNextDisorder(20))
     waitTargetPsn(dut, (100 until 150).toList)
-    
+
     // test 3 bubble
     psnSetter.set(100)
     rxOtherDriver.push(List(100, 102))
@@ -247,64 +263,64 @@ class SeqOutTest extends AnyFunSuite {
     // switch rxOtherResp to rxReadResp, not bubble
     waitTargetPsn(dut, 100)
     dut.clockDomain.waitRisingEdge()
-    assert(dut.io.tx.bth.psn.toInt==101)
+    assert(dut.io.tx.bth.psn.toInt == 101)
     assert(dut.io.tx.valid.toBoolean)
     // switch rxReadResp to rxOtherResp, a bubble
     waitTargetPsn(dut, 101)
     dut.clockDomain.waitRisingEdge()
     assert(!dut.io.tx.valid.toBoolean)
     dut.clockDomain.waitRisingEdge()
-    assert(dut.io.tx.bth.psn.toInt==102)
+    assert(dut.io.tx.bth.psn.toInt == 102)
     assert(dut.io.tx.valid.toBoolean)
   }
 
-  def simSeqOutRandom(dut: SeqOut, bufferDepth:Int): Unit ={
-    simRandomBase(dut, bufferDepth, (rxOtherDriver, rxReadRespDriver, psnGen, packetNum)=>{
-      var nPacketGenerated = 0
-      while(nPacketGenerated<packetNum){
-        var n, m = Random.nextInt(0xFF)
-        if(nPacketGenerated+n+m>packetNum){
-          n = packetNum - nPacketGenerated
-          m = 0
+  def simSeqOutRandom(dut: SeqOut, bufferDepth: Int): Unit = {
+    simRandomBase(
+      dut,
+      bufferDepth,
+      (rxOtherDriver, rxReadDriver, psnGen, packetNum) => {
+        var nPacketGenerated = 0
+        while (nPacketGenerated < packetNum) {
+          var n, m = Random.nextInt(0xff)
+          if (nPacketGenerated + n + m > packetNum) {
+            n = packetNum - nPacketGenerated
+            m = 0
+          }
+          rxOtherDriver.push(psnGen.genNextDisorder(n))
+          rxReadDriver.push(psnGen.genNextOrder(m))
+          nPacketGenerated += n + m
         }
-        rxOtherDriver.push(psnGen.genNextDisorder(n))
-        rxReadRespDriver.push(psnGen.genNextOrder(m))
-        nPacketGenerated += n+m
       }
-    })
+    )
   }
 
-  test("rxOtherResp simple test"){
+  test("rxOtherResp simple test") {
     val bufferDepth = 64
-    SimConfig
-      .withWave
+    SimConfig.withWave
       .compile(new SeqOut(BusWidth.W128, bufferDepth, false))
       .doSim(simRxOtherRespSimple(_, bufferDepth))
   }
 
-  test("rxOtherResp random test"){
-    for(bufferDepth <- List(32, 64, 128, 256, 512)){
-      SimConfig
-        .withWave
+  test("rxOtherResp random test") {
+    for (bufferDepth <- List(32, 64, 128, 256, 512)) {
+      SimConfig.withWave
         .compile(new SeqOut(BusWidth.W128, bufferDepth, false))
-        .doSim{simRxOtherRespRandom(_, bufferDepth)}
+        .doSim { simRxOtherRespRandom(_, bufferDepth) }
     }
   }
 
-  test("SeqOut test"){
+  test("SeqOut test") {
     val bufferDepth = 64
-    SimConfig
-      .withWave
+    SimConfig.withWave
       .compile(new SeqOut(BusWidth.W128, bufferDepth, false))
       .doSim(simSeqOut(_, bufferDepth))
   }
 
-  test("SeqOut test random"){
-    for(bufferDepth <- List(32, 64, 128, 256, 512)){
-      SimConfig
-        .withWave
+  test("SeqOut test random") {
+    for (bufferDepth <- List(32, 64, 128, 256, 512)) {
+      SimConfig.withWave
         .compile(new SeqOut(BusWidth.W128, bufferDepth, false))
-        .doSim{simSeqOutRandom(_, bufferDepth)}
+        .doSim { simSeqOutRandom(_, bufferDepth) }
     }
   }
 }
